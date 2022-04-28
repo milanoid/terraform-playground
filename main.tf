a33cbea2bdbd306b780f4adf7668daef7ac64ee7
Milan Vojnovič <milan.vojnovic@statsperform.com>
2022-04-28 13:32:19 +0200

    statsperform aws account settings

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 terraform {
 
-  # backend for storing terraform state
+  # todo - find out how to authenticate to StatsPerform AWS account in the Terraform Cloud
   # cloud {
   #   organization = "milanvojnovic-org"
   #   workspaces {
@@ -22,13 +22,15 @@ provider "aws" {
   region                   = "eu-west-1" # Ireland
   shared_config_files      = ["/Users/milan/.aws/config"]
   shared_credentials_files = ["/Users/milan/.aws/credentials"]
-  profile                  = "milanoid"
+  profile                  = "apifeeds-dev"
 }
 
 resource "aws_instance" "app_server" {
   ami             = "ami-08ca3fed11864d6bb" # Ubuntu
   #ami             = "ami-0a8dc52684ee2fee2" # Amazon Linux 2
   instance_type   = "t2.micro"
+  # subnet_id - needed as no default VPC assigned to my user@Stats
+  subnet_id       = "subnet-0dfda3416ed62f915"
   tags = {
     Name = var.instance_name
   }
